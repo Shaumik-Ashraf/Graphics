afa5b735dd755459d88e5b9cda323e002a8cd3a2
Shaumik <a.shaumik@gmail.com>
2016-02-05 01:03:43 +0000

    image gallery hw

--- picmaker.c
+++ picmaker.c
@@ -6,10 +6,14 @@
 #include<unistd.h>
 #include<fcntl.h>
 
+#define SQR(x) ((x)*(x))
+#define SQRSUM(x, y) (SQR(x)+SQR(y))
+
 void mkpic0(int fd, char* buffer, int bufmax);
 void mkpic1(int fd, char* buffer, int bufmax);
 void mkpic2(int fd, char* buffer, int bufmax);
 void mkpic3(int fd, char* buffer, int bufmax);
+void mkpic4(int fd, char* buffer, int bufmax);
 
 int main(int argc, char* argv[]) {
 
@@ -91,6 +95,34 @@ void mkpic3(int fd, char* buffer, int bufmax) {
 	for(i=0; i<500; i++) {
 		for(j=0; j<500; j++) {
 			sprintf(buffer, "%i %i %i ", (int)(cos((double)(j*0.07))*255), 0, (int)(sin((double)(i*0.7))*255) );
+			write(fd, buffer, strlen(buffer));
+			memset(buffer, '\0', bufmax);
+	    }
+	    write(fd, "\n", 2);
+	}
+	
+}
+
+void mkpic4(int fd, char* buffer, int bufmax) {
+	
+	int i, j, r=0, g=0, b=0;
+	
+	for(i=0; i<500; i++) {
+		for(j=0; j<500; j++) {
+			
+			if( (SQRSUM(i, j)) < sqrt(300) ) {
+				g = (int)(sin(SQRSUM(i, j) * 3.141592 / 600 )*255);
+			}
+			
+			if( SQRSUM(i, (j-500)) < sqrt(200) ) {
+				r = (int)(sin(SQRSUM(i, j) * 3.141592 / 600 )*255);
+			}
+			
+			if( SQRSUM((i-500), j) < sqrt(150) ) {
+				b = (int)(sin(SQRSUM(i, j) * 3.141592 / 600 )*255);
+			}
+			
+			sprintf(buffer, "%i %i %i ", r, g, b );
 			write(fd, buffer, strlen(buffer));
 			memset(buffer, '\0', bufmax);
 	    }
