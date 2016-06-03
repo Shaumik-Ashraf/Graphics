5bb6e56a1730fcbaa132f6587cb909364fa5e67e
Shaumik-Ashraf <a.shaumik@gmail.com>
2016-06-03 10:15:48 -0400

    started scanline.c

---
+++ final/scanline.c
@@ -0,0 +1,29 @@
+//scanline.c
+//implement scanline conversion
+//Shaumik Ashraf, 6/3/16
+
+#include"scanline.h"
+
+#include"ml6.h"
+#include"zbuf.h"
+
+
+void h_scanline_conversion( screen s, zbuffer zbuf, color c,
+                            double x0, double y0, double z0,
+                            double x1, double y1, double z1,
+                            double x2, double y2, double z2 ) {
+    
+    double btm_x, btm_y, btm_z;
+    double mid_x, mid_y, mid_z;
+    double top_x, top_y, top_z;
+    
+    
+    
+}
+
+void v_scanline_conversion( screen s, zbuffer zbuf, color c,
+                            double x0, double y0, double z0,
+                            double x1, double y1, double z1,
+                            double x2, double y2, double z2 );
+
+
